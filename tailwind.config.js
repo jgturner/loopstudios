8735dc878e09daa27576f2f996594fb6e001153e
jgturner <jgturner.29@gmail.com>
2023-06-01 18:37:13 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alata: ['Alata'],
+      },
+      backgroundImage: {
+        'hero-pattern-desktop': "url('../images/desktop/image-hero.jpg')",
+        'hero-pattern-mobile': "url('../images/mobile/image-hero.jpg')",
+      },
+      letterSpacing: {
+        widest: '.3em',
+      },
+    },
+  },
+  plugins: [],
+};
